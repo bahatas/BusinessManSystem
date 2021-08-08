7acdc74bdcd56631597eb1d80fa1b900efb36e4d
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-08-08 11:45:13 +0100

    Service Impl

--- src/main/java/com/cybertek/business/controller/UserController.java
+++ src/main/java/com/cybertek/business/controller/UserController.java
@@ -1,23 +1,29 @@
 package com.cybertek.business.controller;
 
 
+import com.cybertek.business.dto.RoleDTO;
+import com.cybertek.business.dto.UserDTO;
+import com.cybertek.business.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller@RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
 
-    @GetMapping("/user-create")
-    public String create(){
+    @GetMapping("/create")
+    public String create(Model model){
 
+        model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles",new RoleDTO());
+        model.addAttribute("userList",userService.listAllUser());
         return "pages/user/user-create";
     }
 
-    @GetMapping("project-create")
-    public String createPage(){
 
-        return "pages/user/project-create";
-    }
 }
